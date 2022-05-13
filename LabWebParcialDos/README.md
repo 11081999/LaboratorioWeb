1fa90d0bef646128df59f26c80664d8eecad56fd
11081999 <robertoriverateran@gmail.com>
2022-05-13 18:08:50 -0500

    examen parcial

---
+++ LabWebParcialDos/README.md
@@ -0,0 +1,49 @@
+# Actividad Individual
+ 
+## Getting Started
+ 
+Para correr el siguiente proyecto se deben ejecutar los siguientes comandos dentro de la carpeta del proyecto:
+ 
+Primer comando:
+ 
+    npm i
+ 
+Segundo comando:
+ 
+    npm run devstart
+ 
+NOTA: Es importante tener el servicio de MongoDB corriendo con un esquema llamado "red_bicicletas".
+ 
+## 1. Test unitarios.
+ 
+Se crearon 8 test unitarios con mocha ubicados en la carpeta de spec:
+ 
+ - Archivo de pruebas: mocha_bicicleta_api.spec.js\
+    Pruebas: Obtener el listado de bicicletas, agregar una bicicleta.
+ 
+ -  Archivo de pruebas: mocha_bicicleta_test.spec.js\
+    Pruebas: Crear una bicicleta, listar todas las bicicletas, añadir bicicleta, encontrar bici por código, eliminar bicicleta.
+ 
+ - Archivo de pruebas: mocha_usuario_test.spec.js\
+    Pruebas: Que un usuario reserve una bicicleta.
+ 
+    Las pruebas se pueden realizar y verificar con el siguiente comando:
+ 
+        npm mochatest spec/models/mocha_usuario_test.spec.js
+ 
+ 
+## 2. Autenticación con verificación.
+ 
+1. Se debe de crear un usuario en la [Página de Registro](http://localhost:3000/usuarios/create) o bien accediendo desde index > usuarios > create.
+ 
+2. La verificación se realiza activando un link que se envía por correo a una cuenta de  Ethereal Email.
+ 
+3. Ir a la [Página de Login](https://www.example.com) o bien accediendo desde index > usuarios > Login.
+ 
+NOTA: Solo los usuarios verificados pueden hacer login, en el caso exitoso el sistema redireccionará a la página de usuarios. De lo contrario el sistema arroja un mensaje de error.
+ 
+## 3. Persistencia en MongoDB.
+ 
+- Los usuarios, las bicicletas y las reservaciones persisten en una instancia de MongoDB.
+ 
+- Se pueden ver las reservaciones en la [Página de Reservas](http://localhost:3000/reservas/) o bien accediendo desde index > reservas.
---
+++ LabWebParcialDos/src/React_simple_page/index.js
@@ -0,0 +1,183 @@
+
+
+function Header() {
+    return (
+        <header>
+            <a href="#."><img src="img/aribnb-logo.png" class='airbnb-logo' /></a>
+            <nav>
+                <h1>Become a Host</h1>
+                <img src="img/world.svg" />
+                <div class="user">
+                    <img src="img/menu.svg" class="menu" />
+                    <img src="https://picsum.photos/200" class="user-pic" />
+                </div>
+            </nav>
+        </header>
+    )
+}
+function MainContent() {
+    return (
+        <Cards />
+    )
+}
+
+function Cards() {
+    return(
+        <section class="cards">
+            <h1>Plan a trip with help from local Hosts around the world</h1>
+            <div class="cards-container">
+                <div class="card">
+                    <img src="https://picsum.photos/240/340?r=0" class="card-image" />
+                    <img src="img/heart.svg" class="like" />
+                    <img src="img/star.svg" class="star" /><span class="rating">4.98</span><span class="reviews">(130)</span><span class="country">· United States</span>
+                    <p class="place">Plan The Perfect New York Vacation</p>
+                    <p class="price">From $102 MXN / <span>person</span></p>
+                </div>
+                <div class="card">
+                    <img src="https://picsum.photos/240/340?r=1" class="card-image" />
+                    <img src="img/heart.svg" class="like" />
+                    <img src="img/star.svg" class="star" /><span class="rating">4.98</span><span class="reviews">(130)</span><span class="country">· United States</span>
+                    <p class="place">Plan The Perfect New York Vacation</p>
+                    <p class="price">From $102 MXN / <span>person</span></p>
+                </div>
+                <div class="card">
+                    <img src="https://picsum.photos/240/340?r=2" class="card-image" />
+                    <img src="img/heart.svg" class="like" />
+                    <img src="img/star.svg" class="star" /><span class="rating">4.98</span><span class="reviews">(130)</span><span class="country">· United States</span>
+                    <p class="place">Plan The Perfect New York Vacation</p>
+                    <p class="price">From $102 MXN / <span>person</span></p>
+                </div>
+                <div class="card">
+                    <img src="https://picsum.photos/240/340?r=3" class="card-image" />
+                    <img src="img/heart.svg" class="like" />
+                    <img src="img/star.svg" class="star" /><span class="rating">4.98</span><span class="reviews">(130)</span><span class="country">· United States</span>
+                    <p class="place">Plan The Perfect New York Vacation</p>
+                    <p class="price">From $102 MXN / <span>person</span></p>
+                </div>
+                <div class="card">
+                    <img src="https://picsum.photos/240/340?r=4" class="card-image" />
+                    <img src="img/heart.svg" class="like" />
+                    <img src="img/star.svg" class="star" /><span class="rating">4.98</span><span class="reviews">(130)</span><span class="country">· United States</span>
+                    <p class="place">Plan The Perfect New York Vacation</p>
+                    <p class="price">From $102 MXN / <span>person</span></p>
+                </div>
+                <div class="card">
+                    <img src="https://picsum.photos/240/340?r=5" class="card-image" />
+                    <img src="img/heart.svg" class="like" />
+                    <img src="img/star.svg" class="star" /><span class="rating">4.98</span><span class="reviews">(130)</span><span class="country">· United States</span>
+                    <p class="place">Plan The Perfect New York Vacation</p>
+                    <p class="price">From $102 MXN / <span>person</span></p>
+                </div>
+                <div class="card">
+                    <img src="https://picsum.photos/240/340?r=6" class="card-image" />
+                    <img src="img/heart.svg" class="like" />
+                    <img src="img/star.svg" class="star" /><span class="rating">4.98</span><span class="reviews">(130)</span><span class="country">· United States</span>
+                    <p class="place">Plan The Perfect New York Vacation</p>
+                    <p class="price">From $102 MXN / <span>person</span></p>
+                </div>
+                <div class="card">
+                    <img src="https://picsum.photos/240/340?r=7" class="card-image" />
+                    <img src="img/heart.svg" class="like" />
+                    <img src="img/star.svg" class="star" /><span class="rating">4.98</span><span class="reviews">(130)</span><span class="country">· United States</span>
+                    <p class="place">Plan The Perfect New York Vacation</p>
+                    <p class="price">From $102 MXN / <span>person</span></p>
+                </div>
+                <div class="card">
+                    <img src="https://picsum.photos/240/340?r=8" class="card-image" />
+                    <img src="img/heart.svg" class="like" />
+                    <img src="img/star.svg" class="star" /><span class="rating">4.98</span><span class="reviews">(130)</span><span class="country">· United States</span>
+                    <p class="place">Plan The Perfect New York Vacation</p>
+                    <p class="price">From $102 MXN / <span>person</span></p>
+                </div>
+                <div class="card">
+                    <img src="https://picsum.photos/240/340?r=9" class="card-image" />
+                    <img src="img/heart.svg" class="like" />
+                    <img src="img/star.svg" class="star" /><span class="rating">4.98</span><span class="reviews">(130)</span><span class="country">· United States</span>
+                    <p class="place">Plan The Perfect New York Vacation</p>
+                    <p class="price">From $102 MXN / <span>person</span></p>
+                </div>
+                <div class="card">
+                    <img src="https://picsum.photos/240/340?r=10" class="card-image" />
+                    <img src="img/heart.svg" class="like" />
+                    <img src="img/star.svg" class="star" /><span class="rating">4.98</span><span class="reviews">(130)</span><span class="country">· United States</span>
+                    <p class="place">Plan The Perfect New York Vacation</p>
+                    <p class="price">From $102 MXN / <span>person</span></p>
+                </div>
+                <div class="card">
+                    <img src="https://picsum.photos/240/340?r=11" class="card-image" />
+                    <img src="img/heart.svg" class="like" />
+                    <img src="img/star.svg" class="star" /><span class="rating">4.98</span><span class="reviews">(130)</span><span class="country">· United States</span>
+                    <p class="place">Plan The Perfect New York Vacation</p>
+                    <p class="price">From $102 MXN / <span>person</span></p>
+                </div>
+                <div class="card">
+                    <img src="https://picsum.photos/240/340?r=12" class="card-image" />
+                    <img src="img/heart.svg" class="like" />
+                    <img src="img/star.svg" class="star" /><span class="rating">4.98</span><span class="reviews">(130)</span><span class="country">· United States</span>
+                    <p class="place">Plan The Perfect New York Vacation</p>
+                    <p class="price">From $102 MXN / <span>person</span></p>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+function Footer() {
+    return (
+        <div>
+            <footer>
+                <div class="footer-menu">
+                    <ul>
+                        <li>Support</li>
+                        <li>Help Center</li>
+                        <li>AirCover</li>
+                        <li>Safety information</li>
+                        <li>Supporting people with disabilities</li>
+                        <li>Cancellation options</li>
+                        <li>Our COVID-19 Response</li>
+                        <li>Report a neighborhood concern</li>
+                    </ul>
+                    <ul>
+                        <li>Community</li>
+                        <li>Airbnb.org: disaster relief housing</li>
+                        <li>Support Afghan refugees</li>
+                        <li>Combating discrimination</li>
+                    </ul>
+                    <ul>
+                        <li>Hosting</li>
+                        <li>Try hosting</li>
+                        <li>AirCover for Hosts</li>
+                        <li>Explore hosting resources</li>
+                        <li>Visit our community forum</li>
+                        <li>How to host responsibly</li>
+                    </ul>
+                    <ul>
+                        <li>About</li>
+                        <li>Newsroom</li>
+                        <li>Learn about new features</li>
+                        <li>Letter from our founders</li>
+                        <li>Careers</li>
+                        <li>Investors</li>
+                    </ul>
+                </div>
+                <div class="footer-bottom">
+                    <p>© 2022 Airbnb, Inc. · Privacy · Terms · Sitemap</p>
+                    <p><img src="img/world.svg" class='world' />English (US) Choose a currency $MXN </p>
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <div>
+        <Header />
+
+        <MainContent />
+
+        <Footer />
+    </div>,
+
+    document.getElementById("root")
+)
+
